1d96cfd73f6d445be4d9146d969107583cfbd7e9
Али Рзаев <alirzaev1996@gmail.com>
2017-06-13 15:17:31 +0300

    Add schedule method

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const body_parser = require('body-parser');
 const vyatsu_router = require('./routes/vyatsu')
 const index_router = require('./routes/index')
 const app = express()
@@ -7,6 +8,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(express.static('files'))
+app.use(body_parser.urlencoded({ extended: true }))
 
 app.use('/', index_router)
 app.use('/vyatsu', vyatsu_router)
--- routes/vyatsu.js
+++ routes/vyatsu.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const https = require('https')
+const request = require('request')
 const fs = require('fs')
 const router = express.Router()
+
+const groups_schedule_links = JSON.parse(fs.readFileSync('files/groups.json', {'encoding': 'utf-8'}))
 
 router.get('/', function (req, res) {
   res.send('VyatSU schedule app')
@@ -17,15 +19,21 @@ router.get('/groups.xml', function(req, res) {
     res.send(fs.readFileSync('files/groups.xml', {'encoding': 'utf-8'}))
 })
 
+router.post('/schedule.json', function(req, res) {
+    html_schedule = req.body.html_schedule
+    options = {
+        'url': 'http://rzaevali.pythonanywhere.com/vyatsu/schedule.json',
+        'method': 'POST',
+        'form': { 'html_schedule': html_schedule}
+    }
+    request.post(options, (error, response, body) => {
+        res.send(body)
+    })
+})
+
 router.get('/test', function(req, res) {
-    https.get('https://www.vyatsu.ru/reports/schedule/Group/5741_2.html', load_res => {
-        data = ""
-        load_res.setEncoding('utf-8')
-        load_res.on('data', chunk => {
-            data += chunk
-        }).on('end', () => {
-            res.send(data)
-        })
+    request.get('https://www.vyatsu.ru/reports/schedule/Group/5741_2.html', (error, response, body) => {
+        res.send(body)
     })
 })
 
