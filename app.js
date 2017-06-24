bda9ee9c7a6358c21f356df0f22eddca900797fd
Али Рзаев <alirzaev1996@gmail.com>
2017-06-24 20:51:59 +0300

    Add headers to responses

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 app.use(express.static('files'))
 app.use(body_parser.urlencoded({ extended: true }))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', index_router)
 app.use('/vyatsu', vyatsu_router)
 
