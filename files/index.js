5aef515c05e710d3d736d70e3969f6e1eb750cef
Али Рзаев <alirzaev1996@gmail.com>
2017-06-27 15:50:51 +0300

    Improvements in mobile site; enable custom schedule page

--- files/index.js
+++ files/index.js
@@ -2,21 +2,15 @@
 new Vue({
   el: '#app',
   data: {
-    bellsShown: false,
-    groupsShown: false,
+    bellsVisible: false,
+    groupsVisible: false,
     curGroupID: null,
     season: '1'
   },
   methods: {
-  	showBells: function() {
-  		this.bellsShown = !this.bellsShown
-  	},
-  	showGroups: function() {
-  		this.groupsShown = !this.groupsShown
-  	},
   	openGroupSchedule: function() {
       const isAutumn = document.getElementById("autumn").checked
-  		window.open(`https://www.vyatsu.ru/reports/schedule/Group/${this.curGroupID}_${isAutumn ? '1' : '2'}.html`, '_blank')
+	  window.open(`/mobile/${this.curGroupID}/${isAutumn ? 'autumn' : 'spring'}`, '_blank')
   	}
   }
 })})()
