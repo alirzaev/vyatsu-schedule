466cc2180df5e360ea9c153f285555a90159c61a
Али Рзаев <alirzaev1996@gmail.com>
2017-06-29 15:49:57 +0300

    Code refactoring

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const body_parser = require('body-parser');
+const express       = require('express')
+const body_parser   = require('body-parser');
 const vyatsu_router = require('./routes/vyatsu')
-const index_router = require('./routes/index')
-const app = express()
+const index_router  = require('./routes/index')
+const app           = express()
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || 8080,
+    ip   = process.env.IP   || '0.0.0.0';
 
 app.use(express.static('files'))
 app.use(body_parser.urlencoded({ extended: true }))
@@ -31,4 +31,4 @@ app.set('view engine', 'pug')
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+module.exports = app;
