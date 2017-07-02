0fe06638bfc7159bf2d6b7718f9fecb45be4206b
Али Рзаев <alirzaev1996@gmail.com>
2017-07-02 16:01:06 +0300

    Remove 'postinstall' script

--- test.js
+++
@@ -1,1 +0,0 @@
-import"./index.css";(function(){new Vue({el:"#app",data:{bellsVisible:false,groupsVisible:false,curGroupID:null,season:"1"},methods:{openGroupSchedule:function(){const isAutumn=document.getElementById("autumn").checked;window.open(`/mobile/${this.curGroupID}/${isAutumn?"autumn":"spring"}`,"_blank")}}})})();
