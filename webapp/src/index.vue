706f75479643930dc917a3b2a6a43cc58976a2e5
Али Рзаев <alirzaev1996@gmail.com>
2017-07-06 21:28:42 +0300

    Add simple splash screen

--- webapp/src/index.vue
+++ webapp/src/index.vue
@@ -1,42 +1,47 @@
 ﻿<template>
   <div id="app" class="container">
-    <div type="button" v-on:click="bellsVisible = !bellsVisible" class="btn btn-default btn-block">
-    Звонки
-    </div>
-    <transition name="vsu">
-      <div v-if="bellsVisible" class="well">
-        <div class="list-group">
-          <div class="list-group-item" v-for="item, index in bells">
-            <h4 class="list-group-item-heading">{{ index + 1 + " пара" }}</h4>
-            <p class="list-group-item-text">{{ item[0] + " - " + item[1] }}</p>
+    <div v-if="ready">
+      <div type="button" v-on:click="bellsVisible = !bellsVisible" class="btn btn-default btn-block">
+      Звонки
+      </div>
+      <transition name="vsu">
+        <div v-if="bellsVisible" class="well">
+          <div class="list-group">
+            <div class="list-group-item" v-for="item, index in bells">
+              <h4 class="list-group-item-heading">{{ index + 1 + " пара" }}</h4>
+              <p class="list-group-item-text">{{ item[0] + " - " + item[1] }}</p>
+            </div>
           </div>
         </div>
+      </transition>
+      <div v-on:click="groupsVisible = !groupsVisible" class="btn btn-default btn-block">
+      Группы
       </div>
-    </transition>
-    <div v-on:click="groupsVisible = !groupsVisible" class="btn btn-default btn-block">
-    Группы
-    </div>
-    <transition name="vsu">
-      <div v-if="groupsVisible" class="well">
-        <div class="form-group">
-          <label for="groupSelector">Выберите группу</label>
-          <select v-model="curGroupID" class="form-control" id="groupSelector">
-            <option v-for="group_id, group in groups" v-bind:value="group_id">{{ group }}</option>
-          </select>
+      <transition name="vsu">
+        <div v-if="groupsVisible" class="well">
+          <div class="form-group">
+            <label for="groupSelector">Выберите группу</label>
+            <select v-model="curGroupID" class="form-control" id="groupSelector">
+              <option v-for="group_id, group in groups" v-bind:value="group_id">{{ group }}</option>
+            </select>
+          </div>
+          <div class="btn-group btn-group-justified" data-toggle="buttons">
+            <label class="btn btn-primary active" for="autumn">
+              Осень
+              <input type="radio" name="season" id="autumn" checked="true">
+            </label>
+            <label class="btn btn-primary" for="spring">
+              Весна
+              <input type="radio" name="season" id="spring">
+            </label>
+          </div><br />
+          <a href="#" v-on:click="openGroupSchedule" v-bind:class="{disabled: curGroupID == null}" class="btn btn-success btn-block" role="button">Открыть расписание</a>
         </div>
-        <div class="btn-group btn-group-justified" data-toggle="buttons">
-          <label class="btn btn-primary active" for="autumn">
-            Осень
-            <input type="radio" name="season" id="autumn" checked="true">
-          </label>
-          <label class="btn btn-primary" for="spring">
-            Весна
-            <input type="radio" name="season" id="spring">
-          </label>
-        </div><br />
-        <a href="#" v-on:click="openGroupSchedule" v-bind:class="{disabled: curGroupID == null}" class="btn btn-success btn-block" role="button">Открыть расписание</a>
-      </div>
-    </transition>
+      </transition>
+    </div>
+    <div v-if="!ready">
+      <h1 class="vsu-splashscreen">Расписание студентов ВятГУ</h1>
+    </div>
   </div>
 </template>
 
@@ -102,6 +107,12 @@ export default {
   max-width: 768px;
   padding: 19px;
   font-family: 'Droid Sans', sans-serif;
+}
+
+.vsu-splashscreen {
+  text-align: center;
+  margin-top: 50%;
+  display: block;
 }
 
 .vsu-enter-active, .vsu-leave-active {
