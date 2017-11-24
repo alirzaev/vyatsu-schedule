77a67767a43d26c3734c3c00d01488e35a600eda
Али <alirzaev1996@gmailc.om>
2017-11-24 17:59:48 +0300

    Add scrolling to the current day

--- routes/webapp.js
+++ routes/webapp.js
@@ -1,10 +1,42 @@
 const express = require('express')
-const fs      = require('fs')
 const request = require('request')
 const router  = express.Router()
 
 const DEBUG = process.env.NODE_ENV !== 'production'
 const BASE_URL = DEBUG ? 'http://localhost:8080' : 'https://vyatsuscheduleapi.herokuapp.com'
+
+function parseDate(date) {
+    let day = Number.parseInt(date.slice(0, 2))
+    let month = Number.parseInt(date.slice(2, 4))
+    let year = Number.parseInt(date.slice(4))
+
+    return [day, month, year]
+}
+
+function getCurrentDay(firstDate, secondDate) {
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const OFFSET_MSK = 10800000
+    const MODULO = 1209600000
+    const DAY_IN_MILLISEC = 24 * 3600 * 1000
+
+    const [fDay, fMonth, fYear] = parseDate(firstDate)
+    const [sDay, sMonth, sYear] = parseDate(secondDate)
+
+    firstDate = Date.parse(`${fDay} ${monthNames[fMonth - 1]} ${fYear} 00:00:00 +0300`)
+    secondDate = Date.parse(`${sDay} ${monthNames[sMonth - 1]} ${sYear} 00:00:00 +0300`)
+
+    const currentDate = Date.now() + OFFSET_MSK
+
+    const difference = ((currentDate - firstDate + MODULO) % MODULO) / DAY_IN_MILLISEC >> 0
+    const cWeek = (difference / 7) >> 0
+    const cDay = difference % 7
+
+    if (cWeek < 0 || cWeek > 1 || cDay > 5 || cDay < 0) {
+        return [-1, -1]
+    } else {
+        return [cWeek, cDay]
+    }
+}
 
 router.get('/', (route_req, route_res) => {
     route_res.redirect('/mobile')
@@ -19,7 +51,6 @@ router.get('/mobile', (route_req, route_res) => {
 router.get('/mobile/:group_id/:season', (route_req, route_res) => {
     console.log(`/mobile/${route_req.params.group_id}/${route_req.params.season}`)
 
-    const opts = {'encoding': 'utf-8'}
     const url = `${BASE_URL}/vyatsu/schedule/${route_req.params.group_id}/${route_req.params.season}`
 
     request.get(url, (req_err, req_res, weeks_data) => {
@@ -30,8 +61,17 @@ router.get('/mobile/:group_id/:season', (route_req, route_res) => {
         }
         request.get(`${BASE_URL}/vyatsu/calls`, (req_err, req_res, calls_data) => {
             const calls = JSON.parse(calls_data)
-            const {weeks, group} = JSON.parse(weeks_data)
-            route_res.render('schedule', {weeks: weeks, bells: calls, group: group})
+            const {weeks, group, date_range} = JSON.parse(weeks_data)
+            // const {weeks, group} = JSON.parse(weeks_data)
+            route_res.render(
+                'schedule',
+                {
+                    weeks: weeks,
+                    bells: calls,
+                    group: group,
+                    current: getCurrentDay(date_range[0], date_range[1])
+                }
+            )
         })
     })
 })
