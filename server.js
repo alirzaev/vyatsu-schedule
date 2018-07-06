4bfab685b3c919f9802f37042ef092431496a775
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-06 11:22:56 +0300

    Update files for dokku compatibility

--- server.js
+++ server.js
@@ -6,11 +6,5 @@ const port = process.env.PORT || 8081,
 
 app.use(express.static('dist'));
 
-// Backward compatibility
-app.get('/mobile', (req, res) => res.redirect('/'))
-app.get(
-    '/mobile/:groupId/:season',
-    (req, res) => res.redirect(`/#/schedule/${req.params.groupId}/${req.params.season}`))
-
 app.listen(port, ip);
 console.log(`Server is running on ${ip}:${port}`);
