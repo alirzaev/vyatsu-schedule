57d861036aa07f11bb01511abaafa9f2cb4920d8
AliRzaev <alirzaev1996@gmail.com>
2019-02-04 19:27:47 +0300

    Try to add Yandex.Metrika

--- src/configure.js
+++ src/configure.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const API_URL = process.env.API_URL || 'http://localhost:8080';
+const YANDEX_METRIKA_ID = process.env.YANDEX_METRIKA_ID;
 
 const config = {
-    apiUrl: API_URL
+    apiUrl: API_URL,
+    yandexMetrikaId: YANDEX_METRIKA_ID
 };
 const configPath = path.resolve(__dirname, 'utils/config.js');
 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import VueScrollTo from 'vue-scrollto';
 import VuePageTitle from 'vue-page-title';
+import VueYandexMetrika from 'vue-yandex-metrika';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import config from './utils/config';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.min.css';
@@ -15,6 +17,19 @@ Vue.use(VueScrollTo);
 Vue.use(VuePageTitle, {
     suffix: '| Расписание занятий'
 });
+Vue.use(VueYandexMetrika, {
+    id: config.yandexMetrikaId,
+    router: router,
+    env: process.env.NODE_ENV,
+    debug: process.env.NODE_ENV !== 'production',
+    options: {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        trackHash: true
+    }
+})
 
 new Vue({
     router,
