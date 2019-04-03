f74f6604296df9ba81194a26da647b6fa311a4cb
AliRzaev <alirzaev1996@gmail.com>
2019-04-03 15:41:43 +0300

    Attach "oborona.digital" banner

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@
         <navbar :title="$store.state.global.navBarTitle"></navbar>
         <div class="container">
             <router-view/>
+            <footer class="page-footer font-small pt-3 footer pb-1 standard-container">
+                <div class="content-column">
+                    <a href="https://oborona.digital/" target="_blank">
+                        <img
+                            class="oborona"
+                            src="assets/Against-Runet-Isolation-hor.png"
+                            alt="Я против изоляции Рунета!"/>
+                    </a>
+                    <div class="footer-copyright text-center py-3">© 2018-2019
+                        <a href="https://vk.me/rzaevali">Али Рзаев</a>
+                    </div>
+                </div>
+            </footer>
         </div>
-        <footer class="page-footer font-small pt-3 footer pb-1">
-            <div class="footer-copyright text-center py-3">© 2018-2019
-                <a href="https://vk.me/rzaevali">Али Рзаев</a>
-            </div>
-        </footer>
     </div>
 </template>
 
@@ -27,10 +35,15 @@ export default {
 
 <style scoped lang="sass">
     @import "sass/common"
+    @import "sass/grids/standard"
 
     .container
         padding-top: $vs-navbar-height + 0.6em
 
     .footer
         min-height: $vs-footer-height
+
+    .oborona
+        max-width: 100%
+        border-radius: 0.25em
 </style>
